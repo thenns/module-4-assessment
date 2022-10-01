8e7d6a0c24d00fefec082c69ecacc61baa1b8e3f
thenns <samm77@gmail.com>
2022-10-01 14:34:15 -0600

    added random fortue feature

--- server/controller.js
+++ server/controller.js
@@ -8,6 +8,17 @@ module.exports = {
         let randomCompliment = compliments[randomIndex];
       
         res.status(200).send(randomCompliment);
-    }
+    },
+    getFortune: (req, res) => {
+        const fortunes = [`no snowflake in an avalanche ever feels responsible`,
+                          `if you eat something and nobody sees you eat it, it has no calories`,
+                          `you will marry a professional athlete - if competitive eating can be considered a sport`,
+                          `ask not what your fortune cookie can do for you but what you can do for your fortune cookie`, 
+                          `you are not illiterate`];
+        let randomIndex = Math.floor(Math.random() * fortunes.length);
+        let randomFortune = fortunes[randomIndex];
+
+        res.status(200).send(randomFortune);
+    },
 
 }
